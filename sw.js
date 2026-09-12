4dbc888da78751228752533d3490a443022b7698
ohhh888 <2693426515@qq.com>
2026-09-12 12:35:36 +0800

    数字人完整显示全身含脚（contain+3:4）（全量）

--- sw.js
+++ sw.js
@@ -1,12 +1,12 @@
 /* 菁鹿四股弦 PWA 离线缓存 Service Worker */
-var CACHE = 'julu-siguxian-v11';
+var CACHE = 'julu-siguxian-v12';
 var APP_SHELL = [
   './',
   './index.html',
   './manifest.webmanifest',
   './favicon.svg',
   './polyfills.js',
-  './assets/index-C5WqajbS.js',
+  './assets/index-67C38OdS.js',
   './assets/index-CTduXyua.css',
   './assets/index-CTduXyua.legacy.css',
   './icons/icon-192.png',
